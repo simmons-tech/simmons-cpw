d018bbbe770c18f921c071a21cbe13e13a70dbf3
larsj@mit.edu <simmons-hall@whole-enchilada.mit.edu>
2014-04-07 00:11:36 -0400

    frontend tours display js

--- frontend/cpwtours/js/tours.js
+++ frontend/cpwtours/js/tours.js
@@ -1,22 +1,111 @@
-var BASE_URL = "http://127.0.0.1:8000/tours"
+var BASE_URL = "/cpw/backend/cpwtours/tours"
 var INFO_URL = BASE_URL + "/info"
+var REQUEST_URL = BASE_URL + "/request"
+var CLAIM_URL = BASE_URL + "/claim"
+var START_URL = BASE_URL + "/start"
+
+var REQUESTED = "R";
+var CLAIMED = "C";
+var STARTED = "S";
 
 $(document).ready(function() {
+    var errorP = $('#error-msg');
+    var numToursSpan = $('#num-tours');
+
+    var noDataDiv = $('#no-data');    
+    var noRequestsDiv = $('#no-requests');
+    var unclaimedDiv = $('#unclaimed-request');
+    var unstartedDiv = $('#unstarted-request');
+
+    // Handle AJAX JSON Responses
+    var showGetJsonError = function (jqxhr, textStatus, error) {
+            var err = textStatus + ", " + error;
+	    displayError("Request failed: " + err);
+    };
+
+    var checkResponse = function (json) {
+	if(json['success']) {
+	    // Success
+	    location.reload();
+	} else {
+	    displayError(json['message']);
+	}
+    };
+
+    var sendRequest = function(url) {
+	$.getJSON(url)
+	.done(function(json) {
+	    checkResponse(json);
+	})
+	.fail(showGetJsonError);
+    };
+
+    // Setup request commands. Just getting these urls will change the status.
+    var requestTour = function() {
+	sendRequest(REQUEST_URL);
+    };
+    var claimTour = function() {
+	sendRequest(CLAIM_URL);
+    };
+    var startTour = function() {
+	sendRequest(START_URL);
+    };
+
+    // Link to buttons
+    $("#request_tour_btn").click(function() {
+	requestTour();
+    });
+    $("#unclaimed_claim_tour_btn").click(function() {
+	claimTour();
+    });
+    $("#unclaimed_start_tour_btn").click(function() {
+	startTour();
+    });
+    $("#unstarted_start_tour_btn").click(function() {
+	startTour();
+    });
+
+
+    // Display error message in red.
+    var displayError = function(msg) {
+	errorP.text(msg);
+    };
+
+    var updateDisplay = function(json) {
+	displayError("");
+	var latest_request = json['latest_request'];
+	var numRequests = json['num_requests']
+
+	noDataDiv.addClass("hidden");	
+	noRequestsDiv.addClass("hidden");
+	unclaimedDiv.addClass("hidden");
+	unstartedDiv.addClass("hidden");
+
+	if (!latest_request || latest_request['status'] == STARTED) {
+	    noRequestsDiv.removeClass("hidden");
+	    // This will be tour (num_so_far + 1)
+	    numRequests += 1;
+	} else if (latest_request['status'] == REQUESTED) {
+	    unclaimedDiv.removeClass("hidden");	    
+	} else if (latest_request['status'] == CLAIMED) {
+	    unstartedDiv.removeClass("hidden");	    	    
+	}
+	
+	numToursSpan.text(numRequests);
+    }
     
     var loadData = function() {
         $.getJSON(INFO_URL)
         .done(function(json) {
-            console.log(json);
+	    updateDisplay(json);
         })
-        .fail(function (jqxhr, textStatus, error) {
-            var err = textStatus + ", " + error;
-            console.log("Request Failed: " + err);
-        });
+	.fail(showGetJsonError);
     }
     
     var initApp = function() {
         loadData();
     }
-    
+
     initApp();
+    setInterval(initApp, 5*1000)
 });
