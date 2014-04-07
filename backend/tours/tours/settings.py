b8623437405a824b4047d32bcbacc80809c7ae7a
Lars Johnson <larsj@mit.edu>
2014-04-06 20:55:19 -0400

    Added local settings

--- backend/tours/tours/settings.py
+++ backend/tours/tours/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Database Migrations
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +83,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from local_settings import *
+except ImportError, exp:
+    pass
