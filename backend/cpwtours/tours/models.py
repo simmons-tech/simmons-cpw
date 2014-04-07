de53178655a3e61854c62f5f0650bb4d5b9e857f
Lars Johnson <larsj@mit.edu>
2014-04-06 21:26:18 -0400

    Added TourRequest models

---
+++ backend/cpwtours/tours/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+class TourRequest(models.Model):
+    REQUESTED = 'R'
+    CLAIMED = 'C'
+    STARTED = 'S'
+    STATUS_CHOICES = (
+        (REQUESTED, 'Requested'),
+        (CLAIMED, 'Claimed'),
+        (STARTED, 'Started'),
+    )
+    
+    status = models.CharField(max_length = 1
+                              choices = STATUS_CHOICES,
+                              default = REQUESTED)
+
+    request_time = models.DateTimeField(auto_now_add=True)
+    claim_time = models.DateTimeField(blank=True)
+    start_time = models.DateTimeField(blank=True)
+    
+    def is_unclaimed(self):
+        return self.status == REQUESTED
+
+    def claim_tour(self):
+        if self.status == CLAIMED:
+            raise ValueError("Tour already claimed")
+        if self.status == STARTED:
+            raise ValueError("Tour already startdd")
+        self.claim_time = datetime.now()
+        self.status = CLAIMED
+        self.save()
+
+    def start_tour(self):
+        if self.status == REQUESTED:
+            self.claime_time = datetime.now()
+        if self.status == STARTED:
+            raise ValueError("Tour already startdd")
+        self.start_time = datetime.now()
+        self.status = STARTED
+        self.save()
