7637f60e10b29608cdf0ab1baa665e36c7bb285d
Lars Johnson <larsj@mit.edu>
2014-04-08 14:39:04 -0400

    Added events app

--- backend/cpwevents/cpwevents/settings.py
+++ backend/cpwevents/cpwevents/settings.py
@@ -36,6 +36,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Our Events App
+    'events',
+
+    # DB Migrations
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
